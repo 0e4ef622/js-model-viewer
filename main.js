aa5a7ce2813ac66f5b1f67da350a253db67173b4
Matthew Tran <0e4ef622@gmail.com>
2016-07-16 03:26:41 -0500

    added shader setup code and source

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+var vtxSrc = "attribute vec4 pos;"+
+             "attribute vec3 normal;"+
+
+             "uniform mat4 worldViewMatrix;"+
+             "uniform mat4 modelMatrix;"+
+             "uniform mat3 normalMatrix;"+
+             "uniform vec3 lightDir;"+
+             "uniform vec4 ambient;"+
+
+             "varying lowp vec4 lighting;"+
+
+             "void main() {"+
+                 "gl_Position = worldViewMatrix * modelMatrix * pos;"+
+                 "vec3 norm = normalMatrix * normal;"+
+                 "vec4 diffuse = vec4(vec3(max(dot(norm, -lightDir), 0.0)), 1.0);"+
+                 "lighting = ambient + diffuse;"+
+             "}";
+
+var fragSrc = "uniform lowp vec4 color;"+
+
+              "varying lowp vec4 lighting;"+
+
+              "void main() {"+
+                  "gl_FragColor = color * lighting;"+
+              "}";
+
+
 window.addEventListener("load", function() {
 
     var loop = (window.requestAnimationFrame ||
@@ -9,5 +36,6 @@ window.addEventListener("load", function() {
 
     resizeCanvas(c);
     glSetup(gl);
+    var prgm = shaderSetup(gl, vtxSrc, fragSrc);
 
 });
--- render.js
+++ render.js
@@ -23,3 +23,35 @@ function updateMatrices(canvas, camera, matrices) {
     // TODO
     matrices.worldView;
 }
+
+function shaderSetup(gl, vtxSrc, fragSrc) {
+    var prgm = gl.createProgram();
+
+    var vtx = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vtx, vtxSrc);
+    gl.compileShader(vtx);
+
+    var success = gl.getShaderParameter(vtx, gl.COMPILE_STATUS);
+    if (!success)
+        alert("error compiling vertex shader\n" + gl.getShaderInfoLog(vtx));
+
+    var frag = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(frag, fragSrc);
+    gl.compileShader(frag);
+
+    success = gl.getShaderParameter(frag, gl.COMPILE_STATUS);
+    if (!success)
+        alert("error compiling fragment shader\n" + gl.getShaderInfoLog(frag));
+
+    gl.attachShader(prgm, vtx);
+    gl.attachShader(prgm, frag);
+    gl.linkProgram(prgm);
+    success = gl.getProgramParameter(prgm, gl.LINK_STATUS);
+    if (!success)
+        alert("error linking shader program\n" + gl.getProgramInfoLog(prgm));
+
+    gl.deleteShader(vtx);
+    gl.deleteShader(frag);
+
+    return prgm;
+}
