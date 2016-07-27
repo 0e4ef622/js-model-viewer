3ee074a966af89bfbbe4e88d4d6326fe6fe3a69f
Matthew Tran <0e4ef622@gmail.com>
2016-07-27 12:39:08 -0500

    use ctrl instead of shift to rotate model
    firefox lets you access the right click menu no matter what with shift+rightclick -_-

--- interact.js
+++ interact.js
@@ -1,9 +1,11 @@
 function setupInteract(camera, model) {
 
     var mousedown = false;
+    var mouseButton = 0;
 
     window.addEventListener("mousedown", function(e) {
         mousedown = true;
+        mouseButton = e.button;
         prevPos = {x: e.pageX, y: e.pageY};
         e.preventDefault();
     });
@@ -17,25 +19,24 @@ function setupInteract(camera, model) {
         cz = 0;
     window.addEventListener("mousemove", function(e) {
         if (mousedown) {
-            if (e.shiftKey) {
-                if (e.button == 0) {
+            if (e.ctrlKey) {
+                if (mouseButton == 0) {
                     var x = (new Mat3(camera.mat)).mult(new Vec3(1, 0, 0)).mult(e.movementY/300);
                     var y = (new Mat3(camera.mat)).mult(new Vec3(0, 1, 0)).mult(e.movementX/300);
                     model.rotate(x.x, x.y, x.z).rotate(y.x, y.y, y.z);
-                } else if (e.button == 2) {
+                } else if (mouseButton == 2) {
                     var z = (new Mat3(camera.mat)).mult(new Vec3(0, 0, 1)).mult(-e.movementX/300);
                     model.rotate(z.x, z.y, z.z);
                 }
             } else {
-                if (e.button == 0) {
+                if (mouseButton == 0) {
                     cy -= e.movementY/300;
                     cx -= e.movementX/300;
                     camera.rotate(-e.movementY/300, -e.movementX/300, 0);
-                } else if (e.button == 2) {
+                } else if (mouseButton == 2) {
                     cz += e.movementX/300;
                     camera.rotate(0, 0, e.movementX/300);
                 }
-                //camera.resetRotation().rotate(cy, cx, cz);
             }
         }
     });
